bcd5ae32d53ee9d6212e53a7f187693a152de172
roh <rodrigosousa1105@gmail.com>
2023-07-09 01:47:49 +0100

    Pop-up added when sending a message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import "../styles/contact.css";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
@@ -8,7 +10,7 @@ function Contact() {
 
     const sendEmail = (e) => {
         e.preventDefault();
-
+    
         emailjs
             .sendForm(
                 "service_mq3eekj",
@@ -20,20 +22,40 @@ function Contact() {
                 (result) => {
                     console.log(result.text);
                     e.target.reset();
+                    toast.success('Thank you for leaving a message!', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    });
                 },
                 (error) => {
                     console.log(error.text);
+                    toast.error('Failed to send message. Please try again later.', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
                 }
             );
     };
-
+    
     return (
         <div id="contact">
+            <ToastContainer />
             <form ref={form} onSubmit={sendEmail}>
                 <h1 id="contact-title">CONTACT ME</h1>
-                <input type="text" name="user_name" placeholder="Name" />
-                <input type="email" name="user_email" placeholder="Email" />
-                <textarea name="message" placeholder="Message" />
+                <input type="text" name="user_name" placeholder="Name" required />
+                <input type="email" name="user_email" placeholder="Email" required />
+                <textarea name="message" placeholder="Message" required />
                 <button id="send-but" type="submit">Send</button>
             </form>
             <div className="socials-contact">
