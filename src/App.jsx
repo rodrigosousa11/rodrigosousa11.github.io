75d25a8d0b68732fd17ddffe96f6d1fefa3d5c0c
Rodrigo <96240973+rodrigosousa11@users.noreply.github.com>
2023-08-04 00:08:47 +0100

    projects section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
 
 function App() {
-    return (
-        <div className="App">
-            <Home />
-            <About />
-            <Skills />
-            <Contact />
-            <Footer />
-        </div>
-    );
+	return (
+		<div className="App">
+			<Home />
+			<About />
+			<Skills />
+			<Projects />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "../styles/projects.css";
+import tasky from "../assets/tasky.png";
+import multiclipboard from "../assets/multiclip.jpg";
+import { BsGithub, BsLink45Deg } from "react-icons/bs";
+
+const Projects = () => {
+	const projectData = [
+		{
+			name: "Tasky",
+			description:
+				"Tasky is a web application built using the MERN (MongoDB, Express, React, Node.js) stack. It allows users to manage their tasks by adding, updating (marking as completed or not), and deleting them. The app also features user authentication to secure task management for each individual user.",
+			warning:
+				"Please be patient when using the website, some functions may take a while due to using a free plan.",
+			imageURL: tasky,
+			githubLink: "https://github.com/rodrigosousa11/tasky-todo-mern-app",
+			websiteLink: "https://tasky-7jcp.onrender.com/",
+		},
+		{
+			name: "CLI Multi-Clipboard",
+			description:
+				"This is a Python clipboard manager that allows you to store more than one element copied and load copied elements back to clipboard using command-line commands. It uses a JSON file to store the copied data.",
+			imageURL: multiclipboard,
+			githubLink: "https://github.com/rodrigosousa11/cli-multi-clipboard",
+		},
+	];
+
+	const [hoveredProject, setHoveredProject] = useState(null);
+
+	return (
+		<div className="projects">
+			<h1 id="projects-title">PROJECTS</h1>
+			{projectData.map((project, index) => (
+				<div key={project.name} className="project-item">
+					<div className="project-description">
+						<h3>{project.name}</h3>
+						<div>{project.description}</div>
+						{project.warning && (
+							<div className="project-warning">
+								{project.warning}
+							</div>
+						)}
+					</div>
+					<div className="project-image">
+						<div
+							className="project-image-container"
+							onMouseEnter={() => setHoveredProject(index)}
+							onMouseLeave={() => setHoveredProject(null)}
+						>
+							<img src={project.imageURL} alt={project.name} />
+							{hoveredProject === index && (
+								<div className="project-links">
+									<a
+										href={project.githubLink}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<BsGithub />
+									</a>
+									{project.websiteLink && (
+										<a
+											href={project.websiteLink}
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											<BsLink45Deg />
+										</a>
+									)}
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Projects;
